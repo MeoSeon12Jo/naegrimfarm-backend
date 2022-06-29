8925da72931c4d7009bf6cf9f4aaba90745db661
JaeWan CHOI <wkdudhksl@gamil.com>
2022-06-29 13:16:53 +0900

    ADD - gallery views, serializers

--- gallery/views.py
+++ gallery/views.py
@@ -1,9 +1,14 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from .serializers import PaintingSerializer
+from auction.models import Painting as PaintingModel
 
-# Create your views here.
+
 class GalleryView(APIView):
     
     def get(self, request):
+        paintings = PaintingModel.objects.filter(is_auction=False)
+        painting_serializer = PaintingSerializer(paintings, many=True).data
+
+        return Response(painting_serializer)
         
-        return Response({'msg': 'get 요청 받았습니다'})
