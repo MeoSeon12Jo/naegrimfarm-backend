43c2764d05de019ff60dfcd02626c987ba724298
Roshu2 <kyumin1020@gmail.com>
2022-06-29 19:02:01 +0900

    FIX user.urls,views,serializer naming

--- user/urls.py
+++ user/urls.py
@@ -5,7 +5,7 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView, #기본 JWT access 토큰 발급 view 인증토큰
     TokenRefreshView, # JWT refresh 토큰 발급 view 인증토큰을 재발급받기위한 또 다른 토큰 
 )
-from user.views import SpartaTokenObtainPairView, OnlyAuthenticatedUserView
+from user.views import FarmTokenObtainPairView, OnlyAuthenticatedUserView
 
 
 urlpatterns = [
@@ -14,6 +14,6 @@ urlpatterns = [
     path('logout/', UserApiView.as_view()),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/sparta/token/', SpartaTokenObtainPairView.as_view(), name='sparta_token'),
+    path('api/farm/token/', FarmTokenObtainPairView.as_view(), name='sparta_token'),
     path('api/authonly/', OnlyAuthenticatedUserView.as_view()),
 ]
