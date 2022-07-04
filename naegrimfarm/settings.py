01fd39b781419e77b48c6e96a5a64a50d9ccffbc
JaeWan CHOI <wkdudhksl@gamil.com>
2022-07-04 22:41:50 +0900

    FIX - Clean Code

--- naegrimfarm/settings.py
+++ naegrimfarm/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import local_settings
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -163,10 +162,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-# MEDIA_URL = '/media/'
-# MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-
 
 AUTH_USER_MODEL = 'user.User'
 
