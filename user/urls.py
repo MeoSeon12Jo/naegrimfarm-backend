d0e00e56a1b05cf1a2ea270c9a608cead8cfa972
Roshu2 <kyumin1020@gmail.com>
2022-07-06 02:08:14 +0900

    FIX - import error

--- user/urls.py
+++ user/urls.py
@@ -2,14 +2,13 @@ from django.urls import path
 
 from .views import UserView
 from rest_framework_simplejwt.views import TokenRefreshView # JWT refresh 토큰 발급 view 인증토큰을 재발급받기위한 또 다른 토큰 
-from user.views import FarmTokenObtainPairView, OnlyAuthenticatedUserView, UserPointView
+from user.views import FarmTokenObtainPairView, UserPointView
 
 
 urlpatterns = [
     path('', UserView.as_view()),
     path('api/farm/token/', FarmTokenObtainPairView.as_view(), name='farm_token'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/authonly/', OnlyAuthenticatedUserView.as_view()),
     path('userpoint/', UserPointView.as_view()),
 
 ]
